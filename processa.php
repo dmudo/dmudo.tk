92849c08d2b38c7d7af7cf4cf520f8fbafb6e44a
Dangelo Martins <dangelomartins@gmail.com>
2023-05-24 21:33:17 -0300

    work modal processa

--- processa.php
+++ processa.php
@@ -24,7 +24,7 @@
         $email->addContent(
             "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
         );
-                $sendgrid = new \SendGrid(getenv('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'));
+                $sendgrid = new \SendGrid(getenv('##'));
         try {
             $response = $sendgrid->send($email);
             print $response->statusCode() . "\n";
